edd75cb71c95f7e266662bf3f8331c0f3d39c4b8
lhc1990 <haichaule1990@gmail.com>
2022-02-04 00:09:16 +0900

    init ware house data

--- sql_queries.py
+++ sql_queries.py
@@ -1,136 +1,71 @@
+import configparser
+
+
+# CONFIG
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
-user_table_drop = "DROP TABLE IF EXISTS users;"
-song_table_drop = "DROP TABLE IF EXISTS songs;"
-artist_table_drop = "DROP TABLE IF EXISTS artists;"
-time_table_drop = "DROP TABLE IF EXISTS time;"
+staging_events_table_drop = ""
+staging_songs_table_drop = ""
+songplay_table_drop = ""
+user_table_drop = ""
+song_table_drop = ""
+artist_table_drop = ""
+time_table_drop = ""
 
 # CREATE TABLES
 
-songplay_table_create = """
-    CREATE TABLE IF NOT EXISTS songplays (
-        songplay_id SERIAL PRIMARY KEY,
-        start_time BIGINT REFERENCES time(start_time),
-        user_id INT REFERENCES users(user_id),
-        level VARCHAR,
-        song_id VARCHAR REFERENCES songs(song_id),
-        artist_id VARCHAR REFERENCES artists(artist_id),
-        session_id INT,
-        location VARCHAR,
-        user_agent VARCHAR
-    );
-"""
+staging_events_table_create= ("""
+""")
 
-user_table_create = """
-    CREATE TABLE IF NOT EXISTS users (
-        user_id INT PRIMARY KEY,
-        first_name VARCHAR,
-        last_name VARCHAR,
-        gender VARCHAR,
-        level VARCHAR
-    );
-"""
+staging_songs_table_create = ("""
+""")
 
-song_table_create = """
-    CREATE TABLE IF NOT EXISTS songs (
-        song_id VARCHAR NOT NULL PRIMARY KEY,
-        title VARCHAR(255) NOT NULL,
-        artist_id VARCHAR(255) NOT NULL,
-        year INT,
-        duration FLOAT
-    );
-"""
+songplay_table_create = ("""
+""")
 
-artist_table_create = """
-    CREATE TABLE IF NOT EXISTS artists (
-        artist_id VARCHAR(255) NOT NULL PRIMARY KEY,
-        name VARCHAR NOT NULL,
-        location VARCHAR,
-        latitude FLOAT,
-        longitude FLOAT
-    );
-"""
+user_table_create = ("""
+""")
 
-time_table_create = """
-    CREATE TABLE IF NOT EXISTS time (
-        start_time BIGINT PRIMARY KEY,
-        hour INT,
-        day INT,
-        week INT,
-        month INT,
-        year INT,
-        weekday INT
-    );
-"""
+song_table_create = ("""
+""")
 
-# INSERT RECORDS
+artist_table_create = ("""
+""")
 
-songplay_table_insert = """
-    INSERT INTO songplays (
-        songplay_id, start_time, user_id, level, song_id,artist_id, session_id, location, user_agent
-    )
-    VALUES (DEFAULT,%s, %s, %s, %s, %s, %s, %s, %s)
-    ON CONFLICT DO NOTHING;
-"""
+time_table_create = ("""
+""")
 
-user_table_insert = """
-   INSERT INTO users (
-       user_id,first_name,last_name,gender,level
-   )
-   VALUES (%s, %s, %s, %s, %s)
-   ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
-"""
+# STAGING TABLES
 
-song_table_insert = """
-    INSERT INTO songs (
-        song_id,title,artist_id, year, duration
-    )
-    VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT DO NOTHING;
-"""
+staging_events_copy = ("""
+""").format()
 
-artist_table_insert = """
-    INSERT INTO artists(
-       artist_id,name,location,latitude,longitude
-    )
-    VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT DO NOTHING;
-   
-"""
+staging_songs_copy = ("""
+""").format()
 
+# FINAL TABLES
 
-time_table_insert = """
-    INSERT INTO time(
-      start_time,hour,day,week,month,year,weekday
-    )
-    VALUES (%s, %s, %s, %s, %s,%s, %s)
-    ON CONFLICT DO NOTHING;
-"""
+songplay_table_insert = ("""
+""")
 
-# FIND SONGS
+user_table_insert = ("""
+""")
 
-song_select = """
-   SELECT s.song_id,s.artist_id
-   FROM songs s
-   JOIN artists a
-   ON s.artist_id = a.artist_id
-   WHERE s.title = %s AND a.name = %s AND s.duration = %s
-"""
+song_table_insert = ("""
+""")
+
+artist_table_insert = ("""
+""")
+
+time_table_insert = ("""
+""")
 
 # QUERY LISTS
 
-create_table_queries = [
-    user_table_create,
-    song_table_create,
-    artist_table_create,
-    time_table_create,
-    songplay_table_create,
-]
-drop_table_queries = [
-    songplay_table_drop,
-    user_table_drop,
-    song_table_drop,
-    artist_table_drop,
-    time_table_drop,
-]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
