f1717b5bbb596a4919bb3c62990276297d1d875a
lhc1990 <haichaule1990@gmail.com>
2022-03-27 19:18:00 +0900

    fix comment of method

--- etl.py
+++ etl.py
@@ -4,6 +4,9 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Load data from S3 to staging table
+    """
     for query in copy_table_queries:
         print(query)
         cur.execute(query)
@@ -11,6 +14,10 @@ def load_staging_tables(cur, conn):
 
 
 def insert_tables(cur, conn):
+    """
+    Insert data from staging table to redshift database
+
+    """
     for query in insert_table_queries:
         print(query)
         cur.execute(query)
@@ -18,6 +25,12 @@ def insert_tables(cur, conn):
 
 
 def main():
+    """
+    - Establishes connection with the redshift database and gets
+    cursor to it.
+    - load data to redshift and insert to fact and dimesion tables
+    - Finally, closes the connection. 
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
